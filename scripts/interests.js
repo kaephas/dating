9b6c502786f0608491ac2a543badef71113e4de3
Kaephas <kkain@mail.greenriver.edu>
2019-04-18 21:26:28 -0400

    change 'activities' to 'interests' in interest view; add interests.js to generate checkboxes

---
+++ scripts/interests.js
@@ -0,0 +1,41 @@
+window.onload = function() {
+    document.getElementById('indoor').innerHTML = indoor();
+    document.getElementById('outdoor').innerHTML = outdoor();
+};
+
+function indoor() {
+    let indoorInterests = [
+        "tv", "movies", "cooking", "board games", "puzzles", "reading", "playing cards", "video games"
+    ];
+
+    let output = "";
+    indoorInterests.forEach(function(interest) {
+        output += '<div class="col-md-3">' +
+            '<input type="checkbox" name="indoor[]" id="' + interest + '">' +
+            '<label for="' + interest + '" class="checkbox">' + interest + '</label>' +
+            '</div>';
+    });
+
+    // let sample = '<div class="col-md-3">' +
+    //         '<input type="checkbox" name="indoor[]" id="tv">' +
+    //         '<label for="tv" class="checkbox"> tv</label>' +
+    //         '</div>';
+
+    return output;
+}
+
+function outdoor() {
+    let outdoorInterests = [
+        "hiking", "biking", "swimming", "collecting", "walking", "climbing"
+    ];
+
+    let output = "";
+    outdoorInterests.forEach(function(interest) {
+        output += '<div class="col-md-3">' +
+            '<input type="checkbox" name="outdoor[]" id="' + interest + '">' +
+            '<label for="' + interest + '" class="checkbox">' + interest + '</label>' +
+            '</div>';
+    });
+
+    return output;
+}
