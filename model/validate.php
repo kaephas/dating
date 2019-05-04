94f135cd5108d01f62e6cda3fa0edefcbb8f11cc
Kaephas <kkain@mail.greenriver.edu>
2019-05-03 21:36:25 -0400

    create validation file and setup empty methods

---
+++ model/validate.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kaephas
+ * Date: 5/3/2019
+ * Time: 18:31
+ */
+
+
+// check if alphabetic
+function validName($name) {
+    // required field
+
+}
+
+// check if numeric and 18 - 118 inclusive
+function validAge() {
+    // required field
+
+}
+
+// check if phone is valid
+function validPhone() {
+    // required field
+
+}
+
+// check if email is valid
+function validEmail() {
+    // required field
+
+}
+
+// check if outdoor interests chosen are valid options
+function validOutdoor() {
+    // not required
+
+}
+
+// check if indoor interests chosen are valid options
+function validIndoor() {
+    // not required
+
+}
